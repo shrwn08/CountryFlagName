3abc2487ea450e34031f5ae3d93d7a39c8c80082
shrwn08 <shrwnkumar08@gmail.com>
2024-06-05 17:19:46 +0530

    cards prepared

---
+++ country-flag-with-name/src/App.js
@@ -0,0 +1,13 @@
+import FlagCard from "./Components/FlagCard";
+
+
+
+function App() {
+  return (
+    <div >
+      <FlagCard/>
+    </div>
+  );
+}
+
+export default App;
---
+++ country-flag-with-name/src/Components/FlagCard.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Circularprogress from "@mui/material/CircularProgress";
+import "./card.css";
+
+const Card = ({ item }) => {
+  if (!item) {
+    return <div><Circularprogress /></div>;
+  }
+
+  return (
+    <div className="card">
+      <img src={item.flags.png} alt={item.flags.alt} className="card-image" />
+      <h3>{item.name.common}</h3>
+    </div>
+  );
+};
+
+const FlagCard = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const countryData = await axios.get(
+          "https://restcountries.com/v3.1/all"
+        );
+        setData(countryData.data);
+        console.log(countryData.data);
+      } catch (error) {
+        console.log("unable to fetch data", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if(!data){
+    <Circularprogress/>
+  }
+
+  return (
+    <div className="container">
+      <div className="cards-container">
+        {data.map((item) => (
+          <Card key={item.tld} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FlagCard;
